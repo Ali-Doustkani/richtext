653e0ed789b39aa6ab65aa086fc92b9dcfb39974
Ali <ali.doostkany@gmail.com>
2019-05-27 13:31:18 +0430

    extract paragraph related codes from Richtext.js to RichParagraph.js

---
+++ src/RichParagraph.js
@@ -0,0 +1,127 @@
+import style from './Stylist/Stylist'
+import createDomReader from './DOM/DomReader'
+import { absoluteRange, relativeRange } from './Range'
+import { renderTo } from './DOM/DomWriter'
+import { breakAt } from './Stylist/Break'
+
+function createRichParagraph(rules, editor, model) {
+  const paragraph = document.createElement('p')
+  paragraph.contentEditable = true
+  if (model) {
+    renderTo(paragraph, model)
+  }
+  return toRichParagraph(rules, editor, paragraph)
+}
+
+function toRichParagraph(rules, editor, paragraph) {
+  const read = createDomReader(rules)
+  const w = {
+    create: model => createRichParagraph(rules, editor, model),
+
+    paragraph: () => paragraph,
+
+    style: (styleName, start, end) =>
+      w.render(
+        style({
+          type: rules[styleName],
+          input: read(paragraph),
+          from: start,
+          to: end
+        })
+      ),
+
+    break: () =>
+      breakAt(
+        w.model,
+        relativeRange(paragraph, window.getSelection().getRangeAt(0))
+      ),
+
+    render: model => {
+      renderTo(paragraph, model)
+      return w
+    },
+
+    setPosition: (start, end) => {
+      end = end || start
+      const points = absoluteRange(paragraph, { start, end })
+      const range = document.createRange()
+      range.setStart(points.startContainer, points.startOffset)
+      range.setEnd(points.endContainer, points.endOffset)
+      const sel = window.getSelection()
+      sel.removeAllRanges()
+      sel.addRange(range)
+      return w
+    },
+
+    attach: () => {
+      editor.appendChild(paragraph)
+      return w
+    },
+
+    replaceWith: (p1, p2) => {
+      p1 = p1.paragraph()
+      p2 = p2.paragraph()
+      editor.removeChild(p1)
+      editor.insertBefore(paragraph, p2)
+      editor.removeChild(p2)
+      paragraph.focus()
+      return w
+    },
+
+    addAfter: p => {
+      p = p.paragraph()
+      p.insertAdjacentElement('afterend', paragraph)
+      paragraph.focus()
+      return w
+    },
+
+    get model() {
+      return read(paragraph)
+    },
+
+    get prev() {
+      if (!paragraph.previousSibling) {
+        return null
+      }
+      return toRichParagraph(rules, editor, paragraph.previousSibling)
+    },
+
+    get next() {
+      if (!paragraph.nextSibling) {
+        return null
+      }
+      return toRichParagraph(rules, editor, paragraph.nextSibling)
+    },
+
+    get length() {
+      return paragraph.textContent.length
+    },
+
+    get isCursorAtBeginning() {
+      const range = relativeRange(
+        paragraph,
+        window.getSelection().getRangeAt(0)
+      )
+      return range.start === 0 && range.end === 0
+    },
+
+    get isCursorAtEnd() {
+      const range = relativeRange(
+        paragraph,
+        window.getSelection().getRangeAt(0)
+      )
+      return range.start === w.length && range.end === w.length
+    },
+
+    get isFirst() {
+      return paragraph.previousSibling === null
+    },
+    
+    get isLast() {
+      return paragraph.nextSibling === null
+    }
+  }
+  return w
+}
+
+export { toRichParagraph, createRichParagraph }
--- src/Richtext.js
+++ src/Richtext.js
@@ -1,9 +1,6 @@
-import style from './Stylist/Stylist'
-import createDomReader from './DOM/DomReader'
-import { relativeRange, absoluteRange } from './Range'
-import { breakAt, glue } from './Stylist/Break'
+import { glue } from './Stylist/Break'
 import { standardizeRules } from './DOM/utils'
-import { renderTo, createParagraph } from './DOM/DomWriter'
+import { toRichParagraph, createRichParagraph } from './RichParagraph'
 
 /**
  * It creates a configurator function based on the rules.
@@ -13,21 +10,20 @@ import { renderTo, createParagraph } from './DOM/DomWriter'
 function create(rules) {
   let staySelected = false
   rules = standardizeRules(rules)
-  const readParagraph = createDomReader(rules)
 
   return function(editor) {
-    checkEditor(editor)
-
+    checkEditor(rules, editor)
+    const paragraph = () => toRichParagraph(rules, editor, document.activeElement)
+   
     editor.addEventListener(
       'keydown',
       e => {
         if (e.key === 'Enter') {
-          e.preventDefault() // prevent creating new lines in the same p element
-          handleEnterKey(readParagraph)
+          handleEnterKey(e, paragraph())
         } else if (e.key === 'Backspace') {
-          handleBackspaceKey(e, readParagraph)
+          handleBackspaceKey(e, paragraph())
         } else if (e.key === 'Delete') {
-          handleDeleteKey(e, readParagraph)
+          handleDeleteKey(e, paragraph())
         }
       },
       true
@@ -45,96 +41,54 @@ function create(rules) {
 
     return {
       staySelected: value => (staySelected = value),
-      apply: (start, end, styleName) => {
-        if (active().parentElement !== editor) {
-          return
-        }
-        renderTo(
-          active(),
-          style({
-            type: rules[styleName],
-            input: readParagraph(active()),
-            from: start,
-            to: end
-          })
-        )
-        setPosition(staySelected ? start : end, end)
-      }
+      apply: (start, end, styleName) =>
+        paragraph()
+          .style(styleName, start, end)
+          .setPosition(staySelected ? start : end, end)
     }
   }
 }
 
-function checkEditor(editor) {
+function checkEditor(rules, editor) {
   if (editor.contentEditable === true) {
     throw new Error('the contentEditable of <div> editor must be false')
   }
   if (!editor.children.length) {
-    createParagraph().append(editor)
+    createRichParagraph(rules, editor).attach()
   }
   if (editor.firstChild.nodeName !== 'P') {
     throw new Error('only <p> element is valid inside editor')
   }
 }
 
-function handleBackspaceKey(event, readParagraph) {
-  const range = getRelativeRange()
-  if (range.start !== 0 || range.end !== 0) {
+function handleEnterKey(event, p) {
+  event.preventDefault() // prevent creating new lines in the same p element
+  const [m1, m2] = p.break()
+  p.render(m1)
+    .create(m2)
+    .addAfter(p)
+}
+
+function handleBackspaceKey(event, p) {
+  if (!p.isCursorAtBeginning || p.isFirst) {
     return
   }
-
-  const prevParagarph = active().previousSibling
-  if (prevParagarph === null) {
-    return
-  }
-
   event.preventDefault()
-  const len = prevParagarph.textContent.length
-  const newModel = glue(readParagraph(prevParagarph), readParagraph(active()))
-  createParagraph(newModel).replaceWith(active(), prevParagarph)
-  setPosition(len)
+  const len = p.prev.length
+  p.create(glue(p.prev.model, p.model))
+    .replaceWith(p, p.prev)
+    .setPosition(len)
 }
 
-function handleDeleteKey(event, readParagraph) {
-  const range = getRelativeRange()
-  const len = active().textContent.length
-  if (range.start !== len || range.end !== len) {
+function handleDeleteKey(event, p) {
+  if (!p.isCursorAtEnd || p.isLast) {
     return
   }
-
-  const nextParagraph = active().nextSibling
-  if (nextParagraph === null) {
-    return
-  }
-
+  const len = p.length
   event.preventDefault()
-  const newModel = glue(readParagraph(active()), readParagraph(nextParagraph))
-  createParagraph(newModel).replaceWith(active(), nextParagraph)
-  setPosition(len)
-}
-
-function handleEnterKey(readParagraph) {
-  const [m1, m2] = breakAt(readParagraph(active()), getRelativeRange())
-  renderTo(active(), m1)
-  createParagraph(m2).addAfter(active())
-}
-
-function setPosition(start, end) {
-  end = end || start
-  const points = absoluteRange(active(), { start, end })
-  const range = document.createRange()
-  range.setStart(points.startContainer, points.startOffset)
-  range.setEnd(points.endContainer, points.endOffset)
-  const sel = window.getSelection()
-  sel.removeAllRanges()
-  sel.addRange(range)
-}
-
-function active() {
-  return document.activeElement
-}
-
-function getRelativeRange() {
-  return relativeRange(active(), window.getSelection().getRangeAt(0))
+  p.create(glue(p.model, p.next.model))
+    .replaceWith(p, p.next)
+    .setPosition(len)
 }
 
 export default create
